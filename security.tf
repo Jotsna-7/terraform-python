47a44303e539d964279c3055092c47aeabd98c88
Jotsna-7 <144553257+Jotsna-7@users.noreply.github.com>
2024-11-06 16:43:15 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -3,12 +3,6 @@ resource "aws_security_group" "security1" {
   vpc_id = aws_vpc.demovpc.id
 
 #inbound Rules
-ingress  {
-    from_port = "80"
-    to_port = "80"
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-}
 ingress {
   from_port   = 0
   to_port     = 0
