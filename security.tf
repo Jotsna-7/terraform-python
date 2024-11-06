29c9d17f7e15a2f53aadd2b50b24555f51e11660
Jotsna-7 <144553257+Jotsna-7@users.noreply.github.com>
2024-11-06 16:31:34 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -21,13 +21,12 @@ ingress  {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 }
-egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "all"
     cidr_blocks = ["0.0.0.0/0"]
-
-}
+  }
 tags = {
   Name = "terraform sg"
 }
