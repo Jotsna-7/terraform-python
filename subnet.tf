88a8cea5d7411c886107db07a498f7c5da230dcb
Jotsna-7 <144553257+Jotsna-7@users.noreply.github.com>
2024-11-06 15:11:50 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "public_sb_1" {
     vpc_id = aws_vpc.demovpc.id
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch=true
-    availability_zone = "us-west-1b"
+    availability_zone = "us-east-1a"
     tags = {
       Name="web subnet1"
     }
@@ -11,7 +11,7 @@ resource "aws_subnet" "public_sb_2" {
     vpc_id = aws_vpc.demovpc.id
     cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch=true
-    availability_zone = "us-west-1c"
+    availability_zone = "us-east-1b"
     tags = {
       Name="web subnet2"
     }
@@ -20,7 +20,7 @@ resource "aws_subnet" "application_sb_1" {
     vpc_id = aws_vpc.demovpc.id
     cidr_block = "10.0.3.0/24"
     map_public_ip_on_launch=true
-    availability_zone = "us-west-1b"
+    availability_zone = "us-east-1a"
     tags = {
       Name="application subnet1"
     }
@@ -29,7 +29,7 @@ resource "aws_subnet" "application_sb_2" {
     vpc_id = aws_vpc.demovpc.id
     cidr_block = "10.0.4.0/24"
     map_public_ip_on_launch=true
-    availability_zone = "us-west-1c"
+    availability_zone = "us-east-1b"
     tags = {
       Name="application subnet2"
     }
@@ -38,7 +38,7 @@ resource "aws_subnet" "private_sb_1" {
     vpc_id = aws_vpc.demovpc.id
     cidr_block = "10.0.5.0/24"
     map_public_ip_on_launch=false
-    availability_zone = "us-west-1b"
+    availability_zone = "us-east-1a"
     tags = {
       Name="database subnet1"
     }
@@ -47,7 +47,7 @@ resource "aws_subnet" "private_sb_2" {
     vpc_id = aws_vpc.demovpc.id
     cidr_block = "10.0.6.0/24"
     map_public_ip_on_launch=false
-    availability_zone = "us-west-1c"
+    availability_zone = "us-east-1b"
     tags = {
       Name="database subnet2"
     }
